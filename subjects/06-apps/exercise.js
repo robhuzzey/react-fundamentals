61766280e9b95ede4816366fecc3df53f9222553
Rob Huzzey <robert.huzzey@holidayextras.com>
2015-12-01 16:11:58 +0000

    The giphy stuff

--- subjects/06-apps/exercise.js
+++ subjects/06-apps/exercise.js
@@ -12,6 +12,95 @@
 var React = require('react');
 var { login, sendMessage, subscribeToMessages } = require('./utils/ChatUtils');
 
+
+var Message = React.createClass({
+  render() {
+    return (
+      <li>
+        <p><img src={this.props.data.avatar} width="40" /></p>
+        <p>Message: {this.props.data.text}</p>
+        <p>Name: {this.props.data.username}</p>
+      </li>
+    )
+  }
+});
+
+var TextBox = React.createClass({
+  sendMessage() {
+    sendMessage(
+      'general', // the channel to post a message to, please post to "general" at first
+      this.props.auth.github.displayName, // the github user name
+      this.props.auth.github.profileImageURL, // the github avatar
+      this.refs.input.value // the actual message
+    );
+  },
+  render() {
+    return <div><input ref="input" type="text" /><button onClick={this.sendMessage}>Send</button></div>
+  }
+});
+
+
+var Giphy = function(message) {
+  var search = message.text.match(/\/giphy\s[\w]+/);
+  if(search) {
+    var q = search[0].replace('/giphy ', '');
+
+    fetch(new Request('http://api.giphy.com/v1/gifs/search?q=' + q + '&api_key=dc6zaTOxFJmzC')).then(function(response) { 
+      response.json().then(
+        function(json) {
+          console.log(json.data[0].images.fixed_height_small.url);
+          sendMessage(
+            'general', // the channel to post a message to, please post to "general" at first
+            'Giphy Bot: ' + q, // the github user name
+            json.data[0].images.fixed_height_small.url, // the github avatar
+            '^^^^ @' + message.username +  ' See the avatar ^^^^' // the actual message
+          );
+        }
+      );
+    });
+  }
+};
+
+var App = React.createClass({
+
+  getInitialState() {
+    return {
+      messages: [],
+      unsubscribe: null
+    }
+  },
+
+  componentDidMount() {
+    login((error, auth) => {
+      this.setState({
+        auth
+      });
+      this.unsubscribe = subscribeToMessages('general', (messages) => {
+        Giphy(messages.pop());
+        this.setState({
+          messages
+        })
+      });
+    })
+  },
+
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <TextBox auth={this.state.auth} />
+        <ul>
+          {this.state.messages.reverse().map(function(message) {
+            return <Message data={message} />
+          })}
+        </ul>
+      </div>
+    )
+  }
+});
+
+React.render(<App />, document.getElementById('app'));
+
 /*
 
 Here's how to use the ChatUtils:
